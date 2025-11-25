380da8b010570a9891a339e1de658b7fda58171b
bhaveshrordev <salujabhavesh24@gmail.com>
2025-11-25 21:44:14 +0530

    created api to add user in database

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,17 @@ const port = 3000;
 
 connectToMongo();
 
+app.use(express.json())
+
 // Available Routes
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 
-// app.get('/', (req, res) => {
-//   res.send('Hello from backend');
-// });
+app.get('/', (req, res) => {
+  res.send('Hello from backend');
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
