4a2d4b034e76c2e5ed7a9119c4e207548d946e0e
bhaveshrordev <salujabhavesh24@gmail.com>
2025-11-26 14:10:00 +0530

    added bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
 
 // Create a user using: POST "/api/auth/createuser". No login required
 router.post('/createuser',[
@@ -24,10 +24,13 @@ router.post('/createuser',[
             return res.status(400).json({error: "Sorry a user with this email already exists"})
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         })
         res.json(user)
         
