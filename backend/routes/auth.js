260c5da1c0d037a8ba1dc0adc7542fb5ced234b5
bhaveshrordev <salujabhavesh24@gmail.com>
2026-01-15 19:55:15 +0530

    added signup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -18,10 +18,11 @@ router.post(
     }),
   ],
   async (req, res) => {
+    let success = false;
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
 
     // check whether the user with this email exists already
@@ -31,7 +32,7 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ error: "Sorry a user with this email already exists" });
+          .json({ success, error: "Sorry a user with this email already exists" });
       }
 
       const salt = await bcrypt.genSalt(10);
@@ -46,7 +47,8 @@ router.post(
       const data = { user: { id: user.id } };
       const authtoken = jwt.sign(data, JWT_SECRET);
 
-      res.json({ authtoken });
+      success = true;
+      res.json({ success, authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some error occured");
